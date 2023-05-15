f99d6bea4fe65b9911aab32d3c86c24168766ad5
Ksenia <xenosks@gmail.com>
2023-05-15 22:23:59 +0400

    Add two examples from course

---
+++ is_it_string.py
@@ -0,0 +1,2 @@
+def string_or_not(value):
+    return isinstance(value, str) and 'yes' or 'no'
---
+++ normalize_url.py
@@ -0,0 +1,9 @@
+def normalize_url(url):
+    prefix = 'https://'
+    if url[:8] == prefix:
+        return url
+    else:
+        if url[:7] == 'http://':
+            return prefix + url[7:]
+        else:
+            return prefix + url
